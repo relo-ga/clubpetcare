dff06df7e02119a5a8368aeb82b659fdf4157c28
MrPotatoJr <90863275+MrPotatoJr@users.noreply.github.com>
2025-03-10 00:43:55 +0000

    Quick fix de relaciones y identacion de DB. Adcionalmente estoy trabajando en la ruta para crear usuarios pero tengo un error 405 :(

--- src/api/models.py
+++ src/api/models.py
@@ -12,8 +12,8 @@ class User(db.Model):
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
     location: Mapped[str] = mapped_column(nullable=False)
-    photo: Mapped[str] = mapped_column (nullable=False)
-    phone: Mapped[str] = mapped_column (nullable=False)
+    photo: Mapped[str] = mapped_column(nullable=False)
+    phone: Mapped[str] = mapped_column(nullable=False)
 
     def serialize(self):
         return {
@@ -26,23 +26,41 @@ class User(db.Model):
             "phone": self.phone,
             # Excluir la contraseña por seguridad
         }
+    
+class Company(db.Model):
+    __tablename__ = "company"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name_company: Mapped[str] = mapped_column(nullable=False)
+    email: Mapped[str] = mapped_column(nullable=False)
+    password: Mapped[str] = mapped_column(nullable=False)
+    location: Mapped[str] = mapped_column(nullable=False)
+    photo: Mapped[str] = mapped_column(nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name_company": self.name_company,
+            "email": self.email,
+            "location": self.location,
+            "photo": self.photo,
+            # Excluir la contraseña por seguridad
+        }   
 
 class Pet(db.Model):
     __tablename__ = "pet"
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(50), nullable=False)
-    gender: Mapped[str] = mapped_column (nullable=False)
-    photo: Mapped[str] = mapped_column (nullable=False)
-    medical_history: Mapped[str] = mapped_column (nullable=False)
-    race: Mapped[str] = mapped_column (nullable=False)
-    specie: Mapped[str] = mapped_column (nullable=False)
-    emergency_phone: Mapped[str] = mapped_column (nullable=False)
+    gender: Mapped[str] = mapped_column(nullable=False)
+    photo: Mapped[str] = mapped_column(nullable=False)
+    medical_history: Mapped[str] = mapped_column(nullable=False)
+    race: Mapped[str] = mapped_column(nullable=False)
+    specie: Mapped[str] = mapped_column(nullable=False)
+    emergency_phone: Mapped[str] = mapped_column(nullable=False)
 
 # > Relation One to Many with Pets
 
-    id_user: Mapped[int] = mapped_column (ForeignKey("user.id"))
+    id_user: Mapped[int] = mapped_column(ForeignKey("user.id"))
     user: Mapped["User"] = relationship()
-
 
     def serialize(self):
         return {
@@ -57,35 +75,15 @@ class Pet(db.Model):
             "id_user": self.id_user,
         }
 
-class Company(db.Model):
-    __tablename__ = "company"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name_company: Mapped[str] = mapped_column (nullable=False)
-    email: Mapped[str] = mapped_column (nullable=False)
-    password: Mapped[str] = mapped_column (nullable=False)
-    location: Mapped[str] = mapped_column (nullable=False)
-    photo: Mapped[str] = mapped_column (nullable=False)
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "name_company": self.name_company,
-            "email": self.email,
-            "location": self.location,
-            "photo": self.photo,
-            # Excluir la contraseña por seguridad
-        }
-
 class Services(db.Model):
     __tablename__ = "services"
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column (nullable=False)
+    name: Mapped[str] = mapped_column(nullable=False)
 
 # --> Relation one to many with Company
 
-    id_company: Mapped[int] = mapped_column (ForeignKey("company.id"))
-    user: Mapped["Company"] = relationship()
-
+    id_company: Mapped[int] = mapped_column(ForeignKey("company.id"))
+    company: Mapped["Company"] = relationship()
 
     def serialize(self):
         return {
@@ -96,45 +94,40 @@ class Services(db.Model):
 
 class Favorites(db.Model):
     __tablename__ = "favorites"
-
     id: Mapped[int] = mapped_column(primary_key=True)
 
 # --> Relation one to many with User
 
-    id_user: Mapped[str] = mapped_column (ForeignKey("user.id"))
+    id_user: Mapped[int] = mapped_column(ForeignKey("user.id"))
     user: Mapped["User"] = relationship()
 
 # --> Relation one to many with Company
 
-    id_company: Mapped[str] = mapped_column (ForeignKey("company.id"))
-    user: Mapped["Company"] = relationship()
+    id_company: Mapped[int] = mapped_column(ForeignKey("company.id"))
+    company: Mapped["Company"] = relationship()
 
-
-def serialize(self):
+    def serialize(self):
         return {
             "id": self.id,
             "id_user": self.id_user,
             "id_company": self.id_company,
         }
 
-
 class Appointments(db.Model):
     __tablename__ = "appointments"
     id: Mapped[int] = mapped_column(primary_key=True)
-    date: Mapped[str] = mapped_column (nullable=False)
-    status: Mapped[str] = mapped_column (nullable=False)
-    time: Mapped[str] = mapped_column (nullable=False)
-    location: Mapped[str] = mapped_column (nullable=False)
+    date: Mapped[str] = mapped_column(nullable=False)
+    status: Mapped[str] = mapped_column(nullable=False)
+    time: Mapped[str] = mapped_column(nullable=False)
+    location: Mapped[str] = mapped_column(nullable=False)
 
 # --> Relation Many to many with Pet
-    id_pet: Mapped[str] = mapped_column(ForeignKey("pet.id"))
+    id_pet: Mapped[int] = mapped_column(ForeignKey("pet.id"))
     pet: Mapped["Pet"] = relationship()
 
-
 # --> Relation One to many
-    id_service: Mapped[str] = mapped_column(ForeignKey("service.id"))
+    id_service: Mapped[int] = mapped_column(ForeignKey("services.id"))
     service: Mapped["Services"] = relationship()
-
 
     def serialize(self):
         return {
--- src/api/routes.py
+++ src/api/routes.py
@@ -20,3 +20,19 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+# Se crea la ruta para crear un usuario en la base de datos y se retorna el usuario creado en formato JSON 
+@api.route('/user', methods=['POST'])
+def create_user():
+    request_body = request.get_json()
+    user = User(name=request_body['name'], email=request_body['email'], password=request_body['password'])
+    db.session.add(user)
+    db.session.commit()
+    return jsonify(user.serialize()), 200
+
+# Se crea la ruta para obtener todos los usuarios de la base de datos y se retorna en formato JSON
+@api.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    users = list(map(lambda x: x.serialize(), users))
+    return jsonify(users), 200
