765e70ae3946c22c166e9739e7288d1506fc935f
David Rosas <156865176+davidrosas809@users.noreply.github.com>
2025-03-11 04:01:56 +0000

    david

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -8,7 +8,7 @@ import {
 import { Layout } from "./pages/Layout";
 import { Home } from "./pages/Home";
 import { Single } from "./pages/Single";
-import { Demo } from "./pages/Demo";
+import Registercom from "./pages/Registercom.jsx"
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
@@ -24,7 +24,7 @@ export const router = createBrowserRouter(
         {/* Nested Routes: Defines sub-routes within the BaseHome component. */}
         <Route path= "/" element={<Home />} />
         <Route path="/single/:theId" element={ <Single />} />  {/* Dynamic route for single items */}
-        <Route path="/demo" element={<Demo />} />
+        <Route path="/registercom" element={<Registercom />} />
       </Route>
     )
 );
