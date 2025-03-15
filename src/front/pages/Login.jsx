1549090ff641b9a14184dfe7b156acecafbbce06
relo-ga <40379438+relo-ga@users.noreply.github.com>
2025-03-15 04:44:14 +0000

    login usuarios y empresa

--- src/front/pages/Login.jsx
+++ src/front/pages/Login.jsx
@@ -8,35 +8,84 @@ const Login = () =>{
 
     return(
         <div>
-            <div className="mt-5">
-                <h1 className="text-center"> Login Usuario</h1>
-            </div>
-            <div className="col-4 mx-auto d-flex my-5">
-                <div className="mt-5 col-6">
-                    <img className="mb-4" src="https://cdn-icons-png.flaticon.com/512/4792/4792929.png" alt="" width="150"/>
-                </div>
-                <div className="mt-3 col-6">
-                    <div className="form-floating my-2">
-                        <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
-                        <label for="floatingInput">Email address</label>
-                    </div>
-                    <div className="form-floating my-2">
-                        <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
-                        <label for="floatingPassword">Password</label>
-                    </div>
+            <div className="col-6 mx-auto mt-5">
+                <h1 className="text-center"> Login</h1>
+                
+                <ul className="nav nav-pills nav-fill m-3" id="pills-tab" role="tablist">
+                    <li className="nav-item" role="presentation">
+                        <button className="nav-link active" id="pills-usuario-tab" data-bs-toggle="pill" 
+                        data-bs-target="#pills-usuario" type="button" role="tab" aria-controls="pills-usuario" aria-selected="true">Usuario</button>
+                    </li>
+                    <li className="nav-item" role="presentation">
+                        <button className="nav-link" id="pills-empresa-tab" data-bs-toggle="pill" 
+                        data-bs-target="#pills-empresa" type="button" role="tab" aria-controls="pills-empresa" aria-selected="false">Empresa</button>
+                    </li>
+                </ul>
 
-                    <div className="form-check text-start my-3">
-                        <input className="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault"/>
-                        <label className="form-check-label" for="flexCheckDefault">
-                            Remember me
-                        </label>
+                {/* Usuario */}
+                <div className="tab-content" id="pills-tabContent">
+                    <div className="tab-pane fade show active" id="pills-usuario" role="tabpanel" aria-labelledby="pills-usuario-tab" tabindex="0">
+                        <div className="col-10 mx-auto d-flex my-5">
+                            <div className="d-flex align-items-start col-7">
+                                <img className="p-4 col-12" style={{width:"100%", height:"300px", objectFit:"cover"}} 
+                                src="https://www.debate.com.mx/img/2018/10/20/dog-838281_1920.jpg?__scale=w:360,h:222,t:2,q:100" alt="" />
+                            </div>
+                            <div className="mt-3 col-5">
+                                <div className="form-floating my-2">
+                                    <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
+                                    <label for="floatingInput">Email address</label>
+                                </div>
+                                <div className="form-floating my-2">
+                                    <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
+                                    <label for="floatingPassword">Password</label>
+                                </div>
+
+                                <div className="form-check text-start my-3">
+                                    <input className="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault"/>
+                                    <label className="form-check-label" for="flexCheckDefault">
+                                        Remember me
+                                    </label>
+                                </div>
+                                <button className="btn btn-primary w-100 py-2" type="submit" onClick={ () => navigate("/DashboardUser")}>Sign in</button>
+                                <div>
+                                    <p className="mt-5 mb-3 text-body-secondary">No tienes cuenta? sé parte del club <Link to="/registeruse">aquí.</Link></p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <button className="btn btn-primary w-100 py-2" type="submit" onClick={ () => navigate("/DashboardUser")}>Sign in</button>
-                    <div>
-                        <p className="mt-5 mb-3 text-body-secondary">No tienes cuenta? sé parte del club <Link to="/registeruse">aquí.</Link></p>
-                        <p className="mb-3 text-body-secondary">Eres empresa? ofrece tus servicios <Link to="/registercom">aquí.</Link></p>
+                    {/* Empresa */}
+                    <div className="tab-pane fade" id="pills-empresa" role="tabpanel" aria-labelledby="pills-empresa-tab" tabindex="0">
+                        <div className="col-10 mx-auto d-flex my-5">
+                            <div className="d-flex align-items-start col-7">
+                                <img className="p-4 col-12" style={{width:"100%", height:"300px", objectFit:"cover"}} src="https://tvazteca.brightspotcdn.com/dims4/default/7cee31f/2147483647/strip/true/crop/1280x728+0+0/resize/968x551!/format/webp/quality/90/?url=http%3A%2F%2Ftv-azteca-brightspot.s3.amazonaws.com%2F50%2F5c%2F48bbd95a4528a7f5078f236b5df3%2Fveterinaria-publica-en-mexico.jpg"
+                                    alt=""/>
+                            </div>
+                            <div className="mt-3 col-5">
+                                <div className="form-floating my-2">
+                                    <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
+                                    <label for="floatingInput">Email address</label>
+                                </div>
+                                <div className="form-floating my-2">
+                                    <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
+                                    <label for="floatingPassword">Password</label>
+                                </div>
+
+                                <div className="form-check text-start my-3">
+                                    <input className="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault"/>
+                                    <label className="form-check-label" for="flexCheckDefault">
+                                        Remember me
+                                    </label>
+                                </div>
+                                <button className="btn btn-primary w-100 py-2" type="submit" onClick={ () => navigate("/DashboardUser")}>Sign in</button>
+                                <div>
+                                    <p className="mt-5 mb-3 text-body-secondary">Eres empresa? ofrece tus servicios <Link to="/registercom">aquí.</Link></p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
+
+
             </div>
         </div>
     );
