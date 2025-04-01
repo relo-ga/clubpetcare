5536eb55cc9e6c844e811787f73ef40024997b03
DAGarciaLopez <dagarcialopez5@gmail.com>
2025-04-01 21:21:31 +0000

    PetUpdate done, i added the option to update pet photo

--- src/front/pages/PetUpdate.jsx
+++ src/front/pages/PetUpdate.jsx
@@ -3,6 +3,7 @@ import { useNavigate, useParams } from "react-router-dom";
 import useGlobalReducer from "../hooks/useGlobalReducer";
 
 const PetUpdate = () => {
+
   const { store, dispatch } = useGlobalReducer();
   const { id } = useParams();
   const navigate = useNavigate();
@@ -10,7 +11,7 @@ const PetUpdate = () => {
   const [petData, setPetData] = useState(null);
 
   const [puppy, setPuppy] = useState({
-    photo: "",
+    photo: null,
     emergency_phone: "",
     medical_history: "",
     age: ""
@@ -30,11 +31,11 @@ const PetUpdate = () => {
       }
       const data = await response.json();
       setPetData(data);
- 
+
       dispatch({ type: "pet_info", payload: data });
 
       setPuppy({
-        photo: "",
+        photo: data.photo || "",
         emergency_phone: data.phone || "",
         medical_history: data.medical_history || "",
         age: data.age || ""
@@ -44,14 +45,13 @@ const PetUpdate = () => {
     }
   };
 
-
   useEffect(() => {
     if (!store.pet || (store.pet && String(store.pet.id) !== id)) {
       fetchPetById(id);
     } else {
       setPetData(store.pet);
       setPuppy({
-        photo: "",
+        photo: store.pet.photo || "",
         emergency_phone: store.pet.phone || "",
         medical_history: store.pet.medical_history || "",
         age: store.pet.age || ""
@@ -63,19 +63,52 @@ const PetUpdate = () => {
   const handleChange = (e) => {
     const { name, value, files } = e.target;
     if (name === "photo") {
-      setPuppy((prev) => ({ ...prev, photo: files[0] }));
+      const file = files[0];
+      setPuppy((prev) => ({ ...prev, photo: file }));
     } else {
       setPuppy((prev) => ({ ...prev, [name]: value }));
     }
   };
 
+  const uploadImage = async () => {
+
+    const formData = new FormData();
+    formData.append("file", puppy.photo);
+    formData.append('upload_preset', 'ClubPetCare');
+
+    try {
+      const cloudName = "dqs8bd3ts"
+      const response = await fetch(`https://api.cloudinary.com/v1_1/${cloudName}/image/upload`, {
+        method: "POST",
+        body: formData
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        return data.url;
+      } else {
+        console.log("Error uploading image");
+        alert("There was an error uploading the image ❌");
+        return
+      }
+    } catch (error) {
+      console.log("Error: ", error);
+      alert("There was an error uploading the image ❌");
+    }
+  }
+
   const updatePET = async (petId) => {
     try {
+
+      const photoUrl = puppy.photo ? await uploadImage() : null;
+
       const petDataToUpdate = {
+        ...(photoUrl ? { photo: photoUrl } : {}),
         emergency_phone: puppy.emergency_phone,
         medical_history: puppy.medical_history,
         age: puppy.age,
       };
+
       const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/pet/${petId}`, {
         method: "PUT",
         headers: {
@@ -86,13 +119,23 @@ const PetUpdate = () => {
       });
 
       if (!response.ok) {
-        throw new Error("Error al actualizar la mascota");
+        throw new Error("Failed to update pet");
       }
       navigate("/DashboardUser");
     } catch (error) {
       console.error("Error updating pet:", error);
     }
   };
+
+  const getPetImage = () => {
+    if (puppy.photo) {
+      return URL.createObjectURL(puppy.photo);
+    }
+    if (petData && petData.photo && !puppy.photo) {
+      return petData.photo;
+    }
+    return "https://th.bing.com/th/id/OIP.Nz2KaAaoPwGwAGlOWTuOCAHaHa?w=193&h=193&c=7&r=0&o=5&dpr=1.3&pid=1.7";
+  }
 
   if (!petData) {
     return <div>Cargando información de la mascota...</div>;
@@ -104,111 +147,123 @@ const PetUpdate = () => {
       <div className="d-flex justify-content-center">
 
         <div className="col-md-4 text-center mx-2">
-          <img 
-            src={petData.photo || "https://th.bing.com/th/id/OIP.Nz2KaAaoPwGwAGlOWTuOCAHaHa?w=193&h=193&c=7&r=0&o=5&dpr=1.3&pid=1.7"} 
-            alt="Imagen de perfil" 
-            className="profile-img mb-3 col-5" 
-            style={{ width: 300 }} 
+
+          <img src={getPetImage()}
+            alt="Imagen de mascota" className="profile-img mb-3 col-5" id="petImg" style={{ width: 300 }} />
+          <input type="file" className="form-control mb-3" id="imageUpload" accept="image/*"
+            onChange={handleChange} name="photo"
           />
-          <input 
-            type="file" 
-            className="form-control mb-3" 
-            id="imageUpload" 
-            accept="image/*" 
+          <button className="btn btn-primary" id="cameraBtn" onClick={() => setPuppy({ ...puppy, photo: null })}>
+            <span className="camera-text">Clear Image</span>
+          </button>
+
+        </div>
+        {/* <div className="col-md-4 text-center mx-2">
+          <img
+            src={petData.photo || "https://th.bing.com/th/id/OIP.Nz2KaAaoPwGwAGlOWTuOCAHaHa?w=193&h=193&c=7&r=0&o=5&dpr=1.3&pid=1.7"}
+            alt="Imagen de perfil"
+            className="profile-img mb-3 col-5"
+            style={{ width: 300 }}
+          />
+          <input
+            type="file"
+            className="form-control mb-3"
+            id="imageUpload"
+            accept="image/*"
             name="photo"
             onChange={handleChange}
           />
-        </div>
+      </div> */}
 
-        <div className="col-md-8 d-flex justify-content-around align-items-center mx-2">
-          <div id="profileForm" className="row g-3">
+      <div className="col-md-8 d-flex justify-content-around align-items-center mx-2">
+        <div id="profileForm" className="row g-3">
 
-            <div className="mb-3 col-5">
-              <label htmlFor="name" className="form-label">Name</label>
-              <input 
-                type="text" 
-                className="form-control" 
-                id="name" 
-                value={petData.name || ""} 
-                readOnly
-              />
-            </div>
-
-            <div className="mb-3 col-5">
-              <label htmlFor="emergency_phone" className="form-label">Emergency Phone</label>
-              <input 
-                type="tel" 
-                className="form-control" 
-                id="emergency_phone" 
-                name="emergency_phone"
-                value={puppy.emergency_phone}
-                placeholder={petData.phone || ""}
-                onChange={handleChange}
-              />
-            </div>
-
-            <div className="mb-3 col-5">
-              <label htmlFor="specie" className="form-label">Specie</label>
-              <input 
-                type="text" 
-                className="form-control" 
-                id="specie" 
-                value={petData.specie || ""} 
-                readOnly
-              />
-            </div>
-
-            <div className="mb-3 col-5">
-              <label htmlFor="age" className="form-label">Age</label>
-              <input 
-                type="number" 
-                className="form-control" 
-                id="age" 
-                name="age"
-                value={puppy.age}
-                placeholder={petData.age || ""}
-                onChange={handleChange}
-              />
-            </div>
-
-            <div className="mb-3 col-5">
-              <label htmlFor="race" className="form-label">Race</label>
-              <input 
-                type="text" 
-                className="form-control" 
-                id="race" 
-                value={petData.race || ""} 
-                readOnly
-              />
-            </div>
-
-            <div className="mb-3 col-5">
-              <label htmlFor="medical_history" className="form-label">Medical History</label>
-              <input 
-                type="text" 
-                className="form-control" 
-                id="medical_history" 
-                name="medical_history"
-                value={puppy.medical_history}
-                placeholder={petData.medical_history || ""}
-                onChange={handleChange}
-              />
-            </div>
-            <div class="form-group">
-    <label htmlFor="exampleFormControlFile1">Example file input</label>
-    <input type="file" class="form-control-file" id="exampleFormControlFile1"/>
-  </div>
-            <button 
-              type="submit" 
-              className="btn btn-primary" 
-              onClick={() => updatePET(id)}
-            >
-              Update Changes
-            </button>
+          <div className="mb-3 col-5">
+            <label htmlFor="name" className="form-label">Name</label>
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              value={petData.name || ""}
+              readOnly
+            />
           </div>
+
+          <div className="mb-3 col-5">
+            <label htmlFor="emergency_phone" className="form-label">Emergency Phone</label>
+            <input
+              type="tel"
+              className="form-control"
+              id="emergency_phone"
+              name="emergency_phone"
+              value={puppy.emergency_phone}
+              placeholder={petData.phone || ""}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="mb-3 col-5">
+            <label htmlFor="specie" className="form-label">Specie</label>
+            <input
+              type="text"
+              className="form-control"
+              id="specie"
+              value={petData.specie || ""}
+              readOnly
+            />
+          </div>
+
+          <div className="mb-3 col-5">
+            <label htmlFor="age" className="form-label">Age</label>
+            <input
+              type="number"
+              className="form-control"
+              id="age"
+              name="age"
+              value={puppy.age}
+              placeholder={petData.age || ""}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="mb-3 col-5">
+            <label htmlFor="race" className="form-label">Race</label>
+            <input
+              type="text"
+              className="form-control"
+              id="race"
+              value={petData.race || ""}
+              readOnly
+            />
+          </div>
+
+          <div className="mb-3 col-5">
+            <label htmlFor="medical_history" className="form-label">Medical History</label>
+            <input
+              type="text"
+              className="form-control"
+              id="medical_history"
+              name="medical_history"
+              value={puppy.medical_history}
+              placeholder={petData.medical_history || ""}
+              onChange={handleChange}
+            />
+          </div>
+          <div class="form-group">
+            <label htmlFor="exampleFormControlFile1">Example file input</label>
+            <input type="file" class="form-control-file" id="exampleFormControlFile1" />
+          </div>
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={() => updatePET(id)}
+          >
+            Update Changes
+          </button>
         </div>
       </div>
     </div>
+    </div >
   );
 };
 
