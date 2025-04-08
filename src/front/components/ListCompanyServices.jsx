c9a267faad0fbeb2dad8280f3478d970df71a43d
MrPotatoJr <90863275+MrPotatoJr@users.noreply.github.com>
2025-04-08 02:41:28 +0000

    Refactor ListCompanyServices component: improve structure and readability by organizing JSX elements and ensuring consistent formatting

--- src/front/components/ListCompanyServices.jsx
+++ src/front/components/ListCompanyServices.jsx
@@ -1,14 +1,36 @@
 import React from "react";
 
 const ListCompanyServices = (props) => {
+  return (
+    <div className="list-group-item p-3">
+      <h5 className="sour-gummy-head">Title: {props.service}</h5>
 
-    return(
-        <div className="list-group-item p-3">
-            <h5 className="sour-gummy-head">Title: {props.service}</h5>
-            <p><h5 className="sour-gummy-head">Descripcion:</h5> {props.description}</p>
-            <p><h5 className="sour-gummy-head">Image:</h5> {props.image}</p>
-        </div>
-    );
-}
+      <p>
+        <h5 className="sour-gummy-head">Descripcion:</h5>
+        {props.description}
+      </p>
+
+      <div>
+        <h5 className="sour-gummy-head">Image:</h5>
+        {props.image ? (
+          <img
+            src={props.image}
+            alt="Service"
+            style={{
+              width: "120px",
+              height: "120px",
+              objectFit: "cover",
+              borderRadius: "8px",
+              border: "1px solid #ccc",
+              marginTop: "8px"
+            }}
+          />
+        ) : (
+          <p className="text-muted"><em>No image provided.</em></p>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default ListCompanyServices;
